b76530df906dccec1adaa2cf7f4d10e38d9a3663
Souca Ruxandra-Ioana <ruxysouca@gmail.com>
2026-08-17 09:06:59 +0300

    Implemented Rule of Three for dynamic memory in IoTDevice

--- SmartHome IoT Core/IoTDevice.cpp
+++ SmartHome IoT Core/IoTDevice.cpp
@@ -8,3 +8,33 @@ IoTDevice::IoTDevice(const char* name, int id) : deviceID(id), status(OFFLINE)
 	strcpy(deviceName, name);
 	totalDevices++;
 }
+
+IoTDevice::IoTDevice(const IoTDevice& other)
+{
+	deviceName = new char[strlen(other.deviceName) + 1];
+	strcpy(deviceName, other.deviceName);
+	deviceID = other.deviceID;
+	status = other.status;
+	totalDevices++;
+}
+
+IoTDevice& IoTDevice::operator=(const IoTDevice& other)
+{
+	if (this == &other)
+		return *this;
+
+	delete[] deviceName;
+	deviceName = new char[strlen(other.deviceName) + 1];
+	strcpy(deviceName, other.deviceName);
+
+	deviceID = other.deviceID;
+	status = other.status;
+
+	return *this;
+}
+
+IoTDevice::~IoTDevice()
+{
+	delete[] deviceName;
+	totalDevices--;
+}
