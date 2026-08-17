d9b4fd6a11d3a17da0300d1d5809fa8f80027886
Souca Ruxandra-Ioana <ruxysouca@gmail.com>
2026-08-17 12:43:59 +0300

    Declared sensor measurment methods and diagnostics

--- SmartHome_IoT_Core/Sensor.h
+++ SmartHome_IoT_Core/Sensor.h
@@ -10,8 +10,18 @@ protected:
 
 public:
 	Sensor(const char* name, int id, const char* sensorUnit, float initialReading = 0.0f, float maxThreshold = 50.0f);
-	Sensor(const Sensor& other);
-	Sensor& operator=(const Sensor& other);
-	virtual ~Sensor();
 
+	Sensor(const Sensor& other);
+
+	Sensor& operator=(const Sensor& other);
+
+	void updateReading(float newValue);
+
+	float readValue() const;
+
+	bool isThresholdExceeded() const;
+
+	void runDiagnostics() override;
+
+	virtual ~Sensor();
 };
