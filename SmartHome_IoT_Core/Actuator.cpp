b7577a7e9a138450ce487ab5a39eae1da5311f9a
Souca Ruxandra-Ioana <ruxysouca@gmail.com>
2026-08-17 14:17:15 +0300

    Implemented Actuator methods and memory management in cpp

---
+++ SmartHome_IoT_Core/Actuator.cpp
@@ -0,0 +1,55 @@
+#include "Actuator.h"
+
+Actuator::Actuator(const char* name, int id, const char* type, bool st) : IoTDevice(name, id)
+{
+	actionType = new char[strlen(type) + 1];
+	strcpy(actionType, type);
+
+	state = st;
+}
+
+Actuator::Actuator(const Actuator& other) : IoTDevice(other)
+{
+	actionType = new char[strlen(other.actionType) + 1];
+	strcpy(actionType, other.actionType);
+
+	state = other.state;
+}
+
+Actuator& Actuator::operator=(const Actuator& other)
+{
+	if (this == &other)
+		return *this;
+
+	IoTDevice::operator=(other);
+	
+	delete[] actionType;
+	actionType = new char[strlen(other.actionType) + 1];
+	strcpy(actionType, other.actionType);
+
+	state = other.state;
+
+	return *this;
+}
+
+Actuator::~Actuator()
+{
+	delete[] actionType;
+}
+
+void Actuator::toggleState()
+{
+	state = !state;
+}
+
+bool Actuator::getState() const
+{
+	return state;
+}
+
+void Actuator::runDiagnostics()
+{
+	std::cout << "    DIAGNOSTIC (actuator)   \nName: " << deviceName << "\nID: " << deviceID
+		<< "\nAction Type: " << actionType << "\nState: "
+		<< (state ? "ON\n" : "OFF\n");
+}
